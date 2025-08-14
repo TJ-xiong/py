25d4459019ab589960770ad223d0293dee677b01
tjx <2386584329@qq.com>
2025-08-14 11:25:22 +0800

    火麒麟获取get_subscribe地址改变

--- py脚本/自动获取更新链接.py
+++ py脚本/自动获取更新链接.py
@@ -105,7 +105,7 @@ def get_subscribe(auth_data, jc_type):
             "sec-fetch-site": "cross-site",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
         }
-        u = "https://voop.okyrin.top/api/v1/user/getSubscribe"
+        u = "https://xvonql.pwzvd.cn/api/v1/user/getSubscribe"
     elif jc_type == WebType.XFLTD:
         h = {
             "accept": "application/json, text/plain, */*",
